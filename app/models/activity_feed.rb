f2cc7fadb3cfe24a939cc5baefd29486a4e41c39
Wil Helser <wil@wilhelser.com>
2012-09-14 21:36:23 -0500

    fdfsdf

--- app/models/activity_feed.rb
+++ app/models/activity_feed.rb
@@ -1,5 +1,5 @@
 class ActivityFeed < ActiveRecord::Base
-  attr_accessible: :item_type, :item_id, :item, :actor, :actor_type, :actor_id, :activity 
+  attr_accessible :item_type, :item_id, :item, :actor, :actor_type, :actor_id, :activity 
 
   scope :latest, order("created_at DESC").limit(10)
 
